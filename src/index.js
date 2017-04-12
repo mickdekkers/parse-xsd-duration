9ad4fc574e41b4ffa0fdea4afd21f2f4379de208
Mick Dekkers <mickdekkersnl@gmail.com>
2017-04-12 14:46:14 +0200

    feat: don't process invalid input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export default () => console.log(`Hi!`)
+// Regex taken from https://www.w3.org/TR/xmlschema11-2/#duration-lexical-space
+const isValidXsdDuration = (str) => /^-?P((([0-9]+Y([0-9]+M)?([0-9]+D)?|([0-9]+M)([0-9]+D)?|([0-9]+D))(T(([0-9]+H)([0-9]+M)?([0-9]+(\.[0-9]+)?S)?|([0-9]+M)([0-9]+(\.[0-9]+)?S)?|([0-9]+(\.[0-9]+)?S)))?)|(T(([0-9]+H)([0-9]+M)?([0-9]+(\.[0-9]+)?S)?|([0-9]+M)([0-9]+(\.[0-9]+)?S)?|([0-9]+(\.[0-9]+)?S))))$/.test(str)
+
+export default (input) => {
+  // Throw error for non-string input
+  if (typeof input !== 'string') {
+    throw new TypeError('expected input to be a string')
+  }
+  // Return null for invalid input
+  if (!isValidXsdDuration(input)) return null
+  // TODO: Parse valid input
+}
