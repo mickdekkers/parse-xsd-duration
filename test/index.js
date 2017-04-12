318a3412c29fee451c1be4d75fe58c81bafc124d
Mick Dekkers <mickdekkersnl@gmail.com>
2017-04-12 19:56:30 +0200

    refactor(test): rename output -> expected

--- test/index.js
+++ test/index.js
@@ -6,79 +6,79 @@ const testData = {
   valid: [
     {
       input: 'PT1004199059S',
-      output: 1004199059
+      expected: 1004199059
     },
     {
       input: 'PT130S',
-      output: 130
+      expected: 130
     },
     {
       input: 'PT2M10S',
-      output: 130
+      expected: 130
     },
     {
       input: 'P1DT2S',
-      output: 86402
+      expected: 86402
     },
     {
       input: '-P1Y',
-      output: -31536000
+      expected: -31536000
     },
     {
       input: 'P1Y2M3DT5H20M30.123S',
-      output: 37070430.123
+      expected: 37070430.123
     },
     {
       input: 'P2Y6M5DT12H35M30S',
-      output: 79317330
+      expected: 79317330
     },
     {
       input: 'P1DT2H',
-      output: 93600
+      expected: 93600
     },
     {
       input: 'P20M',
-      output: 52560000
+      expected: 52560000
     },
     {
       input: 'PT20M',
-      output: 1200
+      expected: 1200
     },
     {
       input: 'P0Y20M0D',
-      output: 52560000
+      expected: 52560000
     },
     {
       input: '-P60D',
-      output: -5184000
+      expected: -5184000
     },
     {
       input: 'PT1M30.5S',
-      output: 90.5
+      expected: 90.5
     },
     {
       input: 'P0Y',
-      output: 0
+      expected: 0
     },
     {
       input: 'P0M',
-      output: 0
+      expected: 0
     },
     {
       input: 'P0D',
-      output: 0
+      expected: 0
     },
     {
       input: 'PT0H',
-      output: 0
+      expected: 0
     },
     {
       input: 'PT0M',
-      output: 0
+      expected: 0
     },
     {
       input: 'PT0S',
-      output: 0
+      expected: 0
     }
   ],
   invalid: [
@@ -104,8 +104,8 @@ const testData = {
 testData.invalid.concat(blons)
 
 test('valid values should be parsed correctly', t => {
-  testData.valid.forEach(({ input, output }) => {
-    t.true(pxd(input) === output)
+  testData.valid.forEach(({ input, expected }) => {
+    t.true(pxd(input) === expected)
   })
 })
 
