fb0407c11b53b83788b8775393f7d0620c65f691
Pablo Jomer <Pablo.Jomer@axis.com>
2019-09-30 19:17:57 +0200

    refactor: more reader friendly regex

--- src/index.js
+++ src/index.js
@@ -27,9 +27,8 @@ const emptyTime = {
   [SECOND_UNIT]: 0
 }
 
-// Regex taken from https://www.w3.org/TR/xmlschema11-2/#duration-lexical-space
 const isValidXsdDuration = str =>
-  /^-?P((([0-9]+Y([0-9]+M)?([0-9]+D)?|([0-9]+M)([0-9]+D)?|([0-9]+D))(T(([0-9]+H)([0-9]+M)?([0-9]+(\.[0-9]+)?S)?|([0-9]+M)([0-9]+(\.[0-9]+)?S)?|([0-9]+(\.[0-9]+)?S)))?)|(T(([0-9]+H)([0-9]+M)?([0-9]+(\.[0-9]+)?S)?|([0-9]+M)([0-9]+(\.[0-9]+)?S)?|([0-9]+(\.[0-9]+)?S))))$/.test(
+  /^(-?)P(?=.)((\d+)Y)?((\d+)M)?((\d+)D)?(T(?=.)((\d+)H)?((\d+)M)?(\d*(\.\d+)?S)?)?$/.test(
     str
   )
 const isNonEmptyString = input => typeof input === 'string' && input.length > 0
